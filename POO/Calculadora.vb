ace515d8a7b924a32a405d8f0c179e73f7a6e48c
Mauricio Ramirez <shadow.sfx.sv@gmail.com>
2019-04-07 19:39:35 -0600

    Guia 4 terminada

---
+++ POO/Calculadora.vb
@@ -0,0 +1,30 @@
+﻿Public Class Calculadora
+
+    Public Sub Sumar(a As Double, b As Double)
+        Console.WriteLine(a + b)
+    End Sub
+
+    Public Sub Restar(a As Double, b As Double)
+        Console.WriteLine(a - b)
+    End Sub
+
+    Public Sub Multiplicar(a As Double, b As Double)
+        Console.WriteLine(a * b)
+    End Sub
+
+    Public Sub Dividir(a As Double, b As Double)
+        Console.WriteLine(a / b)
+    End Sub
+
+    Public Sub Todas(a As Double, b As Double)
+        Console.Write("La suma de esos números es: ")
+        Sumar(a, b)
+        Console.Write("La resta de esos números es: ")
+        Restar(a, b)
+        Console.Write("La multiplicacion de esos números es: ")
+        Multiplicar(a, b)
+        Console.Write("La division de esos números es: ")
+        Dividir(a, b)
+    End Sub
+
+End Class
---
+++ POO/CuentaBancaria.vb
@@ -0,0 +1,23 @@
+﻿Public Class CuentaBancaria
+    Private numeroCuenta As Integer
+    Private balance As Double
+
+    Public Sub New(numeroCuenta, balance)
+        Me.numeroCuenta = numeroCuenta
+        Me.balance = balance
+    End Sub
+
+    Public Sub Depositar(monto As Double)
+        balance = balance + monto
+    End Sub
+
+    Public Sub Retirar(monto As Double)
+        balance = balance - monto
+    End Sub
+
+    Public Sub MostrarBalance()
+        Console.WriteLine("Balance: " & balance)
+        Console.ReadLine()
+    End Sub
+
+End Class
